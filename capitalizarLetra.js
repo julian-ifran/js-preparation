8eb53c18918cf843b7ee5d7a7a6e5ca9ec6da521
ifran.softtek <julian.ifran@softtek.com>
2021-08-29 22:42:42 -0300

    Modifico el codigo con un map

--- capitalizarLetra.js
+++ capitalizarLetra.js
@@ -1,17 +1,24 @@
 
-function capitalizarLetra(cadenaADividir){
-    let arrayDeCadenas = cadenaADividir.split(" ");
+// function capitalizarLetra(cadenaADividir){
+//     let arrayDeCadenas = cadenaADividir.split(" ");
 
-    let arrayDeCadenasAux=arrayDeCadenas; //hago una copia que voy a ir reemplazando
-    for (var i=0; i < arrayDeCadenas.length; i++) {
-        let newstring=arrayDeCadenas[i].charAt(0).toUpperCase() + arrayDeCadenas[i].slice(1)
-        arrayDeCadenasAux[i]=newstring
-     }
-     let stringEditado=arrayDeCadenasAux[0];
-     for (var i=1; i < arrayDeCadenasAux.length; i++) {
-         stringEditado= stringEditado + " "+arrayDeCadenasAux[i];
+//     let arrayDeCadenasAux=arrayDeCadenas; //hago una copia que voy a ir reemplazando
+//     for (var i=0; i < arrayDeCadenas.length; i++) {
+//         let newstring=arrayDeCadenas[i].charAt(0).toUpperCase() + arrayDeCadenas[i].slice(1)
+//         arrayDeCadenasAux[i]=newstring
+//      }
+//      let stringEditado=arrayDeCadenasAux[0];
+//      for (var i=1; i < arrayDeCadenasAux.length; i++) {
+//          stringEditado= stringEditado + " "+arrayDeCadenasAux[i];
            
-     }
-    return stringEditado
-}
+//      }
+//     return stringEditado
+// }
 
+//despues de ver en clase las funciones para arrays
+let cadenaADividir="estoy aprendiendo javascript"
+let arrayEdited = cadenaADividir.split(" ");
+let newArray = arrayEdited.map(function(pal) {
+  return pal.charAt(0).toUpperCase() + pal.slice(1);
+});
+console.log(newArray)
